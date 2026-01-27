1d425c8ea3e7cb70aa41d10aa0f9342fb361f170
themarwadrasoi-hue <themarwadrasoi@gmail.com>
2026-01-27 18:38:29 +0530

    Fix cloud build dependencies

--- marwad-native/app.config.js
+++ marwad-native/app.config.js
@@ -23,11 +23,16 @@ export default {
                 "foregroundImage": APP_TYPE === 'delivery' ? "./assets/icon-delivery.png" : "./assets/adaptive-icon.png",
                 "backgroundColor": "#ffffff"
             },
-            "package": `com.marwad.${APP_TYPE}`,
+            "package": `com.marwad.${APP_TYPE || 'admin'}`.toLowerCase(),
             "edgeToEdgeEnabled": true
         },
         "web": {
             "favicon": "./assets/favicon.png"
+        },
+        "extra": {
+            "eas": {
+                "projectId": "30a6377a-bda8-45c9-b605-0bd282000c3e"
+            }
         }
     }
 };
